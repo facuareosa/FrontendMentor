9db7e51066f25c64a1a5fe9c23b68a094efef81a
Facu Areosa <facundo.areosa@gmail.com>
2024-02-15 12:56:43 +0100

    Adaptado css modules/Agregado Qr component challange

---
+++ challanges/qr-Component/QrComponent.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import qrImg from './images/image-qr-code.png'
+import styles from './QrComponent.module.css'
+
+function QrComponent() {
+  return (
+    <div className={styles.app}>
+     <div className={styles.app__container}>
+      <img className={styles.app__img} src={qrImg} alt="image-qr-code"/>
+      <div className={styles.app__textContainer}>
+        <h2 className={styles.app__title}>Improve your front-end skills by building projects</h2>
+        <p className={styles.app__text}>Scan the QR code to vist Frontend Mentor and take your coding skills to the next level</p>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default QrComponent
---
+++ challanges/socialLinksProfile/SocialLinksProfile.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './SocialLinksProfile.module.css'
+
+function SocialLinksProfile() {
+  return (
+    <div className={styles.app}>SocialLinksProfile</div>
+  )
+}
+
+export default SocialLinksProfile
