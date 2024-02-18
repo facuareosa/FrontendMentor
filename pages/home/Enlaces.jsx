808b96205c764de244f37e6c2af98505a1846404
Facu Areosa <facundo.areosa@gmail.com>
2024-02-18 23:00:52 +0100

    agregado const de img en el componente enlaces

--- pages/home/Enlaces.jsx
+++ pages/home/Enlaces.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import homeData from "./homeData.json";
-import styles from "./Enlaces.module.css"
+import styles from "./Enlaces.module.css";
+import qrComponentImg from "../../challanges/qr-Component/assets/images/qrComponentImg.png";
+import socialLinksProfileImg from "../../challanges/socialLinksProfile/assets/images/socialLinksProfileImg.png";
+
+
+
+const images = {
+  qrComponentImg,
+  socialLinksProfileImg
+};
 
 function Enlaces() {
   const desafio = homeData.desafio;
@@ -9,7 +18,7 @@ function Enlaces() {
   const enlaces = desafio.map((data) => (
     <Link className={styles.link} to={data.to}>
       <div className={styles.links__container}>
-        <img className={styles.links__img} src={data.img} alt={`Screenshot de ${data.nombre}`} />
+        <img className={styles.links__img} src={images[data.img]} alt={`Screenshot de ${data.nombre}`} />
         <h2 className={styles.links__title}>{data.nombre}</h2>
       </div>
     </Link>
